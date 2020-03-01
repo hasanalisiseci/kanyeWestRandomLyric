8bf8a613aae4f642d99f7201b41d7ea0b83d031e
HasanAli <53968900+hasanalisiseci@users.noreply.github.com>
2020-03-01 22:41:05 +0300

    Add files via upload
    An app where I showed random Kanye West words on screen with a simple API.

---
+++ KanyeRestAPI/KanyeRestAPI/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  KanyeRestAPI
+//
+//  Created by Hasan Ali on 28.02.2020.
+//  Copyright © 2020 Hasan Ali Şişeci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = "Kanye West Lyrics"
+        imageView.image = UIImage.init(named: "kanye")
+        getLyric()
+        
+    }
+
+    @IBAction func refreshButton(_ sender: Any) {
+        getLyric()
+    }
+    
+
+    func getLyric() {
+        // Hedef URL'yi tanımlıyoruz
+        let url = URL(string: "https://api.kanye.rest/?format=text")
+            do {
+                //URL ile ulaştığımız veriyi String olarak alıyoruz.
+                let contents = try String(contentsOf: url!)
+                //String değeri text view'a gönderiyoruz.
+                textView.text = "\"\(contents)\""
+            } catch {
+                // İçerik yüklenemez ise
+                textView.text = "Could not load"
+            }
+        }
+    }
+    
+
